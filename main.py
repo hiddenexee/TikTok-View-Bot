79c740d43c27ce85d3664ef5b31f9b1a8efffb5c
HiddenExe <161987966+hiddenexee@users.noreply.github.com>
2024-09-30 01:48:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("""
 
-Discord: https://discord.com/users/1213658859185381387\nGmail: hidden4xe@gmail.com
+Discord: https://discord.com/users/1213658859185381387
 Telegram: @hiddenexe
 Gmail: hidden4xe@gmail.com
 
